95a71c4173800de1a949a67b37a1aad279b7d91f
AkashBhatt <akashbhatt@AkashBhatts-MacBook-Air.local>
2023-08-11 12:23:14 +0530

    Basic Setup of the Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="heading">
+        <i class="fa-solid fa-address-book"></i>
+        Your Contact List
+      </h1>
+      <div className="container">
+        <ul type="none" className="contact-list">
+
+          <li>
+            <div className="details-container">
+              <span>
+                <i className="fa-solid fa-user"></i>
+
+              </span>
+              <span></span>
+            </div>
+
+            <a href="/delete_contact/?id=<%=i._id%>" className="delete-btn">
+              <i class="fa-sharp fa-solid fa-xmark" ></i>
+            </a>
+          </li>
+
+        </ul>
+      </div>
+
+      <form action="/add_contact" method="post">
+        <div className="input-box">
+          <input className="input-name" type="text" placeholder="Enter name" required name="name" />
+          <input className="input-phone" type="number" placeholder="Enter Phone" required name="phone" />
+        </div>
+
+        <button className="add-btn" type="submit">Add</button>
+      </form>
+    </>
   );
 }
 
