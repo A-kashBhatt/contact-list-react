7e484a435720f67a67844af0e28cfd5e7b554002
A-kashBhatt <akashbhatt758@gmail.com>
2023-08-11 14:10:22 +0530

    Added contact adding functionality using hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -6,18 +6,23 @@ function App() {
   const [name, setName] = useState('');
   const [contact, setContact] = useState('');
   const [contactList, setContactList] = useState([]);
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // console.log(name);
     // console.log(contact);
+    // console.log(typeof contactList);
     contactList.push({
       name,
       contact
-    })
-    setContactList({
-      contactList
     });
-
+    setContactList(
+      contactList
+    );
+    setName('');
+    setContact('');
+    return;
   }
 
 
@@ -29,28 +34,35 @@ function App() {
       </h1>
       <div className="container">
         <ul type="none" className="contact-list">
+          {
+            // console.log(contactList)
+            contactList.map((contactItem) => {
+              return (
+                <li>
+                  <div className="details-container">
+                    <span>
+                      <i className="fa-solid fa-user"></i>
+                      {contactItem.name}
+                    </span>
+                    <span>{contactItem.contact}</span>
+                  </div>
 
-          <li>
-            <div className="details-container">
-              <span>
-                <i className="fa-solid fa-user"></i>
-                chomu
-              </span>
-              <span>7053541786</span>
-            </div>
+                  <a href="/delete_contact/?id=<%=i._id%>" className="delete-btn">
+                    <i class="fa-sharp fa-solid fa-xmark" ></i>
+                  </a>
+                </li>
+              )
+            })
+          }
 
-            <a href="/delete_contact/?id=<%=i._id%>" className="delete-btn">
-              <i class="fa-sharp fa-solid fa-xmark" ></i>
-            </a>
-          </li>
 
         </ul>
       </div>
 
       <form >
         <div className="input-box">
-          <input className="input-name" type="text" placeholder="Enter name" required name="name" onChange={(e) => setName(e.target.value)} />
-          <input className="input-phone" type="number" placeholder="Enter Phone" required name="phone" onChange={(e) => setContact(e.target.value)} />
+          <input className="input-name" type="text" placeholder="Enter name" value={name} required name="name" onChange={(e) => setName(e.target.value)} />
+          <input className="input-phone" type="number" placeholder="Enter Phone" value={contact} required name="phone" onChange={(e) => setContact(e.target.value)} />
         </div>
 
         <button onClick={handleSubmit} className="add-btn" type="submit">Add</button>
